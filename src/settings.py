0e217f298a4454ca678800a79419743f0f946f29
xudoyberdi123 <71259765+xudoyberdi123@users.noreply.github.com>
2022-05-16 15:39:08 +0300

    Update settings.py
    qwqwe

--- src/settings.py
+++ src/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("DB_NAME", "altech_db"),
-        "USER": os.getenv("DB_USER", "altech_user"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "root"),
-        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / 'db.sqlite3'
     }
 }
 
